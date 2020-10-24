018e6d92df4baed212d77cea5e10648ec0467897
karraspierce09 <58855735+karraspierce09@users.noreply.github.com>
2020-10-23 21:19:42 -0400

    Post Controller,servics, postapost

--- Data/Comments.cs
+++ Data/Comments.cs
@@ -1,26 +1,25 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Data
 {
-    public class Comments
+    public class Comment
     {
         [Key]
         public int CommentId { get; set; }
+        public string Text { get; set; }
 
-        [Required]
-        public string CommentText { get; set; }
+        [ForeignKey("CommentedPost")]
+        public int PostId { get; set; }
+        public virtual Post CommentedPost { get; set; }
 
-        [Required]
-        //public User CommentAuthor { get; set; } // why is this error?
+        public int Likes { get; set; }
 
-        public string Author { get; set; }
-
-        [Required]
-        public Post CommentedPost { get; set; }
+        [ForeignKey("Author")]
+        public Guid UserId { get; set; }
+        public virtual User Author { get; set; }
     }
 }
